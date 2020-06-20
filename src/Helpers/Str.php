4ea1ce179356c4ef05221d53f785764ab310042b
seffeng <seffeng@sina.cn>
2020-06-20 23:08:47 +0800

    增加XML

--- src/Helpers/Str.php
+++ src/Helpers/Str.php
@@ -91,4 +91,43 @@ class Str extends \Illuminate\Support\Str
 
         return $code;
     }
+
+    /**
+     * Safely casts a float to string independent of the current locale.
+     *
+     * The decimal separator will always be `.`.
+     * @param float|int $number a floating point number or integer.
+     * @return string the string representation of the number.
+     */
+    public static function floatToString($number)
+    {
+        // . and , are the only decimal separators known in ICU data,
+        // so its safe to call str_replace here
+        return str_replace(',', '.', strval($number));
+    }
+
+    /**
+     * Returns the trailing name component of a path.
+     * This method is similar to the php function `basename()` except that it will
+     * treat both \ and / as directory separators, independent of the operating system.
+     * This method was mainly created to work on php namespaces. When working with real
+     * file paths, php's `basename()` should work fine for you.
+     * Note: this method is not aware of the actual filesystem, or path components such as "..".
+     *
+     * @param string $path A path string.
+     * @param string $suffix If the name component ends in suffix this will also be cut off.
+     * @return string the trailing name component of the given path.
+     */
+    public static function basename($path, $suffix = '')
+    {
+        if (($len = mb_strlen($suffix)) > 0 && mb_substr($path, -$len) === $suffix) {
+            $path = mb_substr($path, 0, -$len);
+        }
+        $path = rtrim(str_replace('\\', '/', $path), '/\\');
+        if (($pos = mb_strrpos($path, '/')) !== false) {
+            return mb_substr($path, $pos + 1);
+        }
+
+        return $path;
+    }
 }
