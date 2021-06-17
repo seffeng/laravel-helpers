fd0ee6aa21a2b5ed8c5567c2e3ff7aa4ca360156
seffeng <seffeng@sina.cn>
2021-06-17 11:00:59 +0800

    增加加密解密命令

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,36 @@
 ### 更新日志
 
+* 2021-06-17
+
+  * 增加控制台加密解密命令
+
+  ```php
+  # 配置
+  文件 app/Console/Kernel.php 添加代码
+  
+  protected $commands = [
+      // ...
+      \Seffeng\LaravelHelpers\Commands\Crypt::class,
+  ];
+  ```
+
+  ```shell
+  # 示例，加密字符串 aa 或 文件内字符串
+  $ php artisan crypt aa
+  $ php artisan crypt ./aa.txt
+  
+  # 示例，解密被加密字符串 或 文件内字符串
+  $ php artisan crypt --decrypt eyJpdiI6Ik5sMXA....
+  $ php artisan crypt --decrypt ./aa.txt
+  
+  # 帮助
+  $ php artisan crypt -h
+  
+  # 注意
+  1、.env 文件必须存在配置项：APP_KEY=
+  2、本命令使用 .env 文件 APP_KEY 作为加密key，若 APP_KEY 为空时会自动生成 APP_KEY；
+  ```
+---
 
 * 2020-07-21
 
---
+++ src/Commands/Crypt.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Seffeng\LaravelHelpers\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Encryption\Encrypter;
+use Illuminate\Console\ConfirmableTrait;
+use Illuminate\Contracts\Encryption\DecryptException;
+
+class Crypt extends Command
+{
+    use ConfirmableTrait;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crypt
+                        {value : 待加密或解密的字符串或文件}
+                        {--decrypt : 是否解密，无此选项时为加密}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '根据APP_KEY加密解密数据，会自动生成APP_KEY';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        try {
+            $this->initKey();
+            $value = $this->argument('value');
+            $decrypt = $this->option('decrypt');
+            is_file($value) && $value = file_get_contents($value);
+
+            if ($decrypt) {
+                $this->info('解密: ' . decrypt($value));
+            } else {
+                $this->info('加密: ' . encrypt($value));
+            }
+        } catch (DecryptException $e) {
+            $this->error('解密失败：无效的加密数据！');
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     *
+     * @author zxf
+     * @date   2021年1月12日
+     * @return boolean
+     */
+    protected function initKey()
+    {
+        if (strlen($this->laravel['config']['app.key']) === 0) {
+            $key = $this->generateRandomKey();
+            if (!$this->setKeyInEnvironmentFile($key)) {
+                return false;
+            }
+            $this->laravel['config']['app.key'] = $key;
+        }
+        return true;
+    }
+
+
+    /**
+     * Generate a random key for the application.
+     *
+     * @return string
+     */
+    protected function generateRandomKey()
+    {
+        return 'base64:'.base64_encode(
+            Encrypter::generateKey($this->laravel['config']['app.cipher'])
+            );
+    }
+
+
+    /**
+     * Set the application key in the environment file.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    protected function setKeyInEnvironmentFile($key)
+    {
+        $currentKey = $this->laravel['config']['app.key'];
+
+        if (strlen($currentKey) !== 0 && (! $this->confirmToProceed())) {
+            return false;
+        }
+
+        $this->writeNewEnvironmentFileWith($key);
+
+        return true;
+    }
+
+    /**
+     * Write a new environment file with the given key.
+     *
+     * @param  string  $key
+     * @return void
+     */
+    protected function writeNewEnvironmentFileWith($key)
+    {
+        file_put_contents($this->envPath(), preg_replace(
+            $this->keyReplacementPattern(),
+            'APP_KEY='.$key,
+            file_get_contents($this->envPath())
+            ));
+    }
+
+    /**
+     * Get a regex pattern that will match env APP_KEY with any random key.
+     *
+     * @return string
+     */
+    protected function keyReplacementPattern()
+    {
+        $escaped = preg_quote('='.$this->laravel['config']['app.key'], '/');
+
+        return "/^APP_KEY{$escaped}/m";
+    }
+
+    /**
+     *
+     * @author zxf
+     * @date   2021年1月12日
+     * @return string
+     */
+    protected function envPath()
+    {
+        if (method_exists($this->laravel, 'environmentFilePath')) {
+            return $this->laravel->environmentFilePath();
+        }
+
+        return $this->laravel->basePath('.env');
+    }
+}
