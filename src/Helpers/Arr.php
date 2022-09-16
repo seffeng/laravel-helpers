8bede76a1b9e7a008c9287a3fbad1c893758d24a
seffeng <seffeng@sina.cn>
2022-09-16 16:25:01 +0800

    update.

--- src/Helpers/Arr.php
+++ src/Helpers/Arr.php
@@ -10,4 +10,18 @@ use Seffeng\ArrHelper\Traits\ArrTrait;
 class Arr extends \Illuminate\Support\Arr
 {
     use ArrTrait;
+
+    /**
+     *
+     * @author zxf
+     * @date   2022年9月16日
+     * @param array $array
+     * @param string|integer $key
+     * @param mixed $default
+     * @return mixed|array|object
+     */
+    public static function get($array, $key, $default = null)
+    {
+        return static::getValue($array, $key, $default);
+    }
 }
